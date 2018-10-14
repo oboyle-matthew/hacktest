15511397200360a699145d146a0760a872fdfd7f
Matt O <mo121@duke.edu>
2018-10-13 23:57:15 -0400

    Detecting numbers from notes played

--- App.js
+++ App.js
@@ -6,16 +6,20 @@ import {Player} from 'react-native-audio-player-recorder-no-linking';
 
 // import image from "./assets/splash.png";
 
+var faceLocations = [];
+
 export default class App extends React.Component {
     state = {
         hasCameraPermission: null,
         type: Camera.Constants.Type.back,
         image: null,
+        faces: 0,
     };
 
     async componentWillMount() {
         const { status } = await Permissions.askAsync(Permissions.CAMERA);
         await Audio.setIsEnabledAsync(true);
+        this.snap();
         this.setState({ hasCameraPermission: status === 'granted' });
     }
 
@@ -23,10 +27,8 @@ export default class App extends React.Component {
         if (this.camera) {
             let photo = await this.camera.takePictureAsync({base64: true, quality: 0, exif: true});
             console.log(photo);
-            this.setState({image: photo});
-            // getPixelRGBA(image, 0, 0)
-            //     .then(color => console.log(color)) // [243, 123, 0]
-            //     .catch(err => console.log(err));
+            // this.setState({image: photo});
+            setTimeout(this.snap, 0);
         }
     };
 
@@ -40,36 +42,66 @@ export default class App extends React.Component {
         })
     };
 
+    playSound = async (note) => {
+
+    }
+
     handlePlay = async (faceData) => {
 
-        // var totalFaces = 8;
-        // console.log(faceData.faces.length);
-
+        var num = 5;
         const soundObject = new Expo.Audio.Sound();
-        if (faceData.faces.length > 1) {
-            try {
-                console.log("YES");
-                // var num = faceData.faces[0].faceID;
-                await soundObject.loadAsync(require('./sounds/test1.mp3'));
-
-                // (num % 2 === 0) ? await soundObject.loadAsync(require('./sounds/test0.wav')) : await soundObject.loadAsync(require('./sounds/test1.mp3'));
-
-                { shouldPlay: true }
-
-                this.audioPlayer6  = soundObject;
-
-                this.audioPlayer6.playAsync();
-
-                this.audioPlayer6.setPositionAsync(0);
-
-                // this.audioPlayer6.setRateAsync(num*4, false);
-
-                // Your sound is playing!
-
-            } catch (error) {
-                console.log(error);
-                // An error occurred!
+        // console.log(faceData.faces.length);
+        if (this.state.faces === num) {
+            if (faceData.faces.length === num) {
+                faceLocations = faceData.faces.map(face => face.bounds.origin);
             }
+            if (faceData.faces.length < num) {
+                try {
+                    faceLocations.forEach((location, index) => {
+                        if (faceData.faces.filter(face => {
+                            return Math.abs(location.x - face.bounds.origin.x) <= 5
+                        }).length === 0) {
+                            var numSmaller = 0;
+                            faceLocations.forEach(compareToLocation => {
+                                if (location.x > compareToLocation.x) {
+                                    numSmaller += 1;
+                                }
+                            });
+                            console.log(numSmaller);
+                        }
+                    });
+                    faceLocations = [];
+                    this.setState({
+                        faces: faceData.faces.length
+                    });
+                    // var num = faceData.faces[0].faceID;
+                    await soundObject.loadAsync(require('./sounds/test1.mp3'));
+
+                    // (num % 2 === 0) ? await soundObject.loadAsync(require('./sounds/test0.wav')) : await soundObject.loadAsync(require('./sounds/test1.mp3'));
+
+                    { shouldPlay: true }
+
+                    this.audioPlayer6  = soundObject;
+
+                    this.audioPlayer6.playAsync();
+
+                    this.audioPlayer6.setPositionAsync(0);
+
+                    // this.audioPlayer6.setRateAsync(num*4, false);
+
+                    // Your sound is playing!
+
+                } catch (error) {
+                    console.log(error);
+                    // An error occurred!
+                }
+            } else {
+
+            }
+        } else if (faceData.faces.length > this.state.faces) {
+            this.setState({
+                faces: faceData.faces.length
+            })
         }
     }
 
